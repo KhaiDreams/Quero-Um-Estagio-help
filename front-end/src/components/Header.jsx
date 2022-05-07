dd9eadf53672e0a98afdc522458ac37066237bcf
Bruno Capitelli <bruno.nitro48@gmail.com>
2022-05-07 20:01:57 -0300

    initial structure

---
+++ front-end/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import Navbar from "./Navbar"
+
+export default function Header() {
+    return (
+        <>
+            <Navbar />
+            <h1>HEADER</h1>
+        </>
+    )
+}
---
+++ front-end/src/pages/index.jsx
@@ -0,0 +1,11 @@
+import TemplateDefault from "../templates/Default"
+
+export default function Home() {
+    return (
+        <TemplateDefault>
+            <div>
+                HOME
+            </div>
+        </TemplateDefault>
+    )
+  } 
