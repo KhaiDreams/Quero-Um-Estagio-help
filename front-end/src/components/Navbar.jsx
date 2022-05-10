a121ff0821d914e34d5ca868c19aff2c90ed932e
eusolomax <capitellivictor@gmail.com>
2022-05-10 10:28:45 -0300

    first commit

--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -1,5 +1,43 @@
+import Link from 'next/link';
+
+import styles from '../styles/navbar.module.css';
+
 export default function Navbar() {
     return (
-        <h1>NAVBAR</h1>
+        <>
+            <nav className={styles.background}>
+                <div className={styles.menutexto_container}>
+                    <h1 className={styles.menutexto}>MENU</h1>
+                </div>
+
+                    <div className={styles.menuall}>
+                        <Link href="/">
+                            <ul className={styles.menuitens_ul}>
+                                <li className={styles.menuitens_li}>Home</li>
+                            </ul>
+                        </Link>
+
+
+                        <Link href="/">
+                            <ul className={styles.menuitens_ul}>
+                                <li className={styles.menuitens_li}>About</li>
+                            </ul>
+                        </Link>
+
+
+                        <Link href="/">
+                            <ul className={styles.menuitens_ul}>
+                                <li className={styles.menuitens_li}>Projects</li>
+                            </ul>
+                        </Link>
+
+                        <Link href="/">
+                            <ul className={styles.menuitens_ul}>
+                                <li className={styles.menulogin_li}>Login</li>
+                            </ul>
+                        </Link>
+                    </div>
+            </nav>
+        </>
     )
 }
