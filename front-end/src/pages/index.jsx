03562b575d9ad5564d70dc04cb84f5d68bb61f83
eusolomax <capitellivictor@gmail.com>
2022-05-10 16:09:25 -0300

    navbar v1

--- front-end/src/pages/index.jsx
+++ front-end/src/pages/index.jsx
@@ -1,11 +1,15 @@
 import TemplateDefault from "../templates/Default"
+import styles from "../styles/home.module.css"
 
 export default function Home() {
     return (
         <TemplateDefault>
-            <div>
-                HOME
+
+            <div className={styles.div_q_u_e}>
+                <h1>QUERO UM</h1>
+                <h1>ESTAGIO.HELP</h1>
             </div>
+
         </TemplateDefault>
     )
 }
