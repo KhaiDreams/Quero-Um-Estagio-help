e76cdc970dceb45e8c28895a152eea6695e00179
eusolomax <capitellivictor@gmail.com>
2022-05-11 00:18:49 -0300

    letreiro gif + glow letreiro +

--- front-end/src/pages/index.jsx
+++ front-end/src/pages/index.jsx
@@ -1,21 +1,29 @@
 import TemplateDefault from "../templates/Default"
+
+import gif from '../public/mouse.gif';
+
 import styles from "../styles/home.module.css"
+
 
 export default function Home() {
     return (
         <TemplateDefault>
             <div className={styles.background}>
                 <div className={styles.container_top}>
-                    <div className={styles.container_q_u_e}>
 
+
+                    <div className={styles.container_q_u_e}>
                         <h1 className={styles.h1_q_u_e}>QUERO UM</h1>
                         <h1 className={styles.h1_q_u_e}>ESTAGIO <span className={styles.help_color}>.HELP</span> </h1>
                     </div>
+                    
 
+                    <div className={styles.container_gif}>
+                        <img src={gif.src} alt="" className={styles.gif_mouse}/>
+                    </div>  
                     <div className={styles.container_por_favor}>
-
                         <h1 className={styles.h1_por_favor}>POR FAVOR...</h1>
-                    </div>                    
+                    </div>                  
 
                 </div>
             </div>
