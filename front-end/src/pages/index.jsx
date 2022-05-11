dccfa891a62966ae4f161ecabe0f465f57a5b68a
eusolomax <capitellivictor@gmail.com>
2022-05-10 23:47:09 -0300

    navbar + home letreiro + bugs navbar + new scrollbar +

--- front-end/src/pages/index.jsx
+++ front-end/src/pages/index.jsx
@@ -4,10 +4,20 @@ import styles from "../styles/home.module.css"
 export default function Home() {
     return (
         <TemplateDefault>
+            <div className={styles.background}>
+                <div className={styles.container_top}>
+                    <div className={styles.container_q_u_e}>
 
-            <div className={styles.div_q_u_e}>
-                <h1>QUERO UM</h1>
-                <h1>ESTAGIO.HELP</h1>
+                        <h1 className={styles.h1_q_u_e}>QUERO UM</h1>
+                        <h1 className={styles.h1_q_u_e}>ESTAGIO <span className={styles.help_color}>.HELP</span> </h1>
+                    </div>
+
+                    <div className={styles.container_por_favor}>
+
+                        <h1 className={styles.h1_por_favor}>POR FAVOR...</h1>
+                    </div>                    
+
+                </div>
             </div>
 
         </TemplateDefault>
